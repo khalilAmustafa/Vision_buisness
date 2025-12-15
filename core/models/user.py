05b56093f83e7d2391625dee6229bbe3efa5bb32
Raed Khalil <202311019@students.asu.edu.jo>
2025-12-16 02:48:18 +0300

    Delete core directory

--- core/models/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, id, name, username, password_hash, role):
-        self.id = id
-        self.name = name
-        self.username = username
-        self.password_hash = password_hash
-        self.role = role
